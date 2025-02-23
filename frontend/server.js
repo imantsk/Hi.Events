9f3ce7b815a31f1b8d7ca68b5957fef413586cd7
Imants <imants.kokis@gmail.com>
2025-02-23 20:50:04 +0200

    feat: Increase rate limit to 500 requests per 15 minutes

--- frontend/server.js
+++ frontend/server.js
@@ -35,7 +35,7 @@ const app = express();
 // set up rate limiter: maximum of 100 requests per 15 minutes
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // max 100 requests per windowMs
+    max: 500, // max 100 requests per windowMs
 });
 
 // apply rate limiter to all requests
